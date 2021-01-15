5bc6e9f5239fc9f92ebc01bc0614c7a42420eb88
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-01-15 08:42:44 +0200

    refactor(index): set entry point as app

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import ReactDOM from "react-dom";
 import React from "react";
-import gon from "gon";
 
 import "../assets/application.scss";
-import Channels from "./components/Channels";
+import App from "./containers/App";
 
 if (process.env.NODE_ENV !== "production") {
   localStorage.debug = "chat:*";
 }
 
-ReactDOM.render(
-  <Channels list={gon.channels} />,
-  document.getElementById("chat")
-);
+ReactDOM.render(<App />, document.getElementById("chat"));
