98f4768df6665097e53204f05ca5c02d15689772
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-01-15 08:55:12 +0200

    feat(components): make navigation from channels

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,34 @@
+// @ts-check
+
+import React from "react";
+import PropTypes from "prop-types";
+
+const Navigation = ({ items }) => (
+  <ul className="nav flex-column nav-pills nav-fill">
+    {items.map(({ id, name }) => (
+      <li key={id} className="nav-item">
+        <button
+          type="button"
+          className="nav-link mb-2 text-left btn btn-primary btn-block"
+        >
+          {name}
+        </button>
+      </li>
+    ))}
+  </ul>
+);
+
+Navigation.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+    })
+  ),
+};
+
+Navigation.defaultProps = {
+  items: [],
+};
+
+export default Navigation;
