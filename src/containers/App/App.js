410528224c879e3e6e09df65570402c51585d77c
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-01-15 08:56:31 +0200

    feat(containers): add markup and plus button for app

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,10 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import gon from "gon";
 
-import Channels from "../../components/Channels";
+import Navigation from "../../components/Navigation";
 
-const App = () => {
-  return <Channels list={gon.channels} />;
-};
+const App = () => (
+  <div className="row h-100 pb-3">
+    <div className="col-3 border-right">
+      <div className="d-flex mb-2">
+        <span>Channels</span>
+        <button type="button" className="ml-auto p-0 btn btn-link">
+          +
+        </button>
+      </div>
+      <Navigation items={gon.channels} />
+    </div>
+    <main className="col h-100"></main>
+  </div>
+);
 
 export default App;
