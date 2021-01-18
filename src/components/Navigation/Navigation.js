f31a50f097fb4ee8cc4705606311a91c7c5a0222
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-01-18 20:49:46 +0200

    feat(components): add onclick for bavigation's items

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,32 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Navigation = ({ items }) => (
-  <ul className="nav flex-column nav-pills nav-fill">
-    {items.map(({ id, name }) => (
-      <li key={id} className="nav-item">
-        <button
-          type="button"
-          className="nav-link mb-2 text-left btn btn-primary btn-block"
-        >
-          {name}
-        </button>
-      </li>
-    ))}
-  </ul>
-);
+const Navigation = ({ items, onClick }) => {
+  const handleClick = (name) => () => onClick(name);
+
+  return (
+    <ul className="nav flex-column nav-pills nav-fill">
+      {items.map(({ id, active, name }) => (
+        <li key={id} className="nav-item">
+          <button
+            type="button"
+            className={`nav-link mb-2 text-left btn btn-block ${
+              active ? "btn-primary" : "btn-light"
+            }`}
+            onClick={handleClick(name)}
+          >
+            {name}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 Navigation.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
+      active: PropTypes.bool,
       name: PropTypes.string,
     })
   ),
+  onClick: PropTypes.func,
 };
 
 Navigation.defaultProps = {
   items: [],
+  onClick: () => {},
 };
 
 export default Navigation;
