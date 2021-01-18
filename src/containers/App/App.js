46eb827e789ca59df49817983f5c2cf5eb868df4
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-01-18 20:50:13 +0200

    feat(containers): set active channel for app

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import gon from "gon";
 
 import Navigation from "../../components/Navigation";
 
-const App = () => (
-  <div className="row h-100 pb-3">
-    <div className="col-3 border-right">
-      <div className="d-flex mb-2">
-        <span>Channels</span>
-        <button type="button" className="ml-auto p-0 btn btn-link">
-          +
-        </button>
+const App = ({ channels }) => {
+  const [activeChannel, setActiveChannel] = useState("general");
+
+  const items = channels.map(({ name, ...rest }) => ({
+    active: name === activeChannel,
+    name,
+    ...rest,
+  }));
+
+  const handleChannelClick = (name) => setActiveChannel(name);
+
+  return (
+    <div className="row h-100 pb-3">
+      <div className="col-3 border-right">
+        <div className="d-flex mb-2">
+          <span>Channels</span>
+          <button type="button" className="ml-auto p-0 btn btn-link">
+            +
+          </button>
+        </div>
+        <Navigation items={items} onClick={handleChannelClick} />
       </div>
-      <Navigation items={gon.channels} />
+      <main className="col h-100"></main>
     </div>
-    <main className="col h-100"></main>
-  </div>
-);
+  );
+};
+
+App.propTypes = {
+  channels: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+    })
+  ).isRequired,
+};
 
 export default App;
