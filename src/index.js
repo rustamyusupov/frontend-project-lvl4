7d19403b3543de23930caad03ef572d0be226bc4
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-01-18 20:48:15 +0200

    refactor(index): set channles for app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-// @ts-check
-
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import ReactDOM from "react-dom";
 import React from "react";
+import gon from "gon";
 
 import "../assets/application.scss";
 import App from "./containers/App";
@@ -12,4 +11,7 @@ if (process.env.NODE_ENV !== "production") {
   localStorage.debug = "chat:*";
 }
 
-ReactDOM.render(<App />, document.getElementById("chat"));
+ReactDOM.render(
+  <App channels={gon.channels} />,
+  document.getElementById("chat")
+);
