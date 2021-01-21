9a9d599d8f47335962f28fd703851f2f0b503496
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-01-21 21:24:26 +0200

    feat(modules): init user context

---
+++ src/modules/User/context.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import getUserName from "./getUserName";
+
+const UserContext = React.createContext();
+
+const useUser = () => {
+  const context = React.useContext(UserContext);
+
+  if (!context) {
+    throw new Error(`useUser must be used within a UserProvider`);
+  }
+
+  return context;
+};
+
+const UserProvider = (props) => {
+  const name = getUserName();
+  const value = { name };
+
+  return <UserContext.Provider value={value} {...props} />;
+};
+
+export { UserProvider, useUser };
