e1c32f005a675be57549b778eead79fb1382372e
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-01-24 14:04:20 +0200

    refactor(components): optimize chat render

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,17 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Chat = ({ messages }) => {
-  return (
-    <div id="messages-box" className="chat-messages overflow-auto mb-3">
-      {messages.map(({ userName, value }) => (
-        <div className="text-break">
-          <b>{userName}</b>: {value}
-        </div>
-      ))}
-    </div>
-  );
-};
+const Chat = ({ messages }) => (
+  <div id="messages-box" className="chat-messages overflow-auto mb-3">
+    {messages.map(({ userName, value }) => (
+      <div className="text-break">
+        <b>{userName}</b>: {value}
+      </div>
+    ))}
+  </div>
+);
 
 Chat.propTypes = {
   messages: PropTypes.arrayOf(
