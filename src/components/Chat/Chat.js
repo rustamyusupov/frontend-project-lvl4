8f6150d89a16d73c90c92dfcab1b270637b68bcb
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-01-24 14:03:47 +0200

    feat(components): add chat

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Chat = ({ messages }) => {
+  return (
+    <div id="messages-box" className="chat-messages overflow-auto mb-3">
+      {messages.map(({ userName, value }) => (
+        <div className="text-break">
+          <b>{userName}</b>: {value}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+Chat.propTypes = {
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      channelId: PropTypes.number,
+      userName: PropTypes.string,
+      value: PropTypes.string,
+    })
+  ),
+};
+
+Chat.defaultProps = {
+  messages: [],
+};
+
+export default Chat;
