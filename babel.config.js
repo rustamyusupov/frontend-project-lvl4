d132218099acf6e8a3b98c0b15e786562ee6081b
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-01-31 15:30:39 +0200

    chore(babel): config module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+      },
+    ],
   ],
 };
