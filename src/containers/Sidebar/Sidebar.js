f627737adbbfd15dca2180a0d29f8c65dd6b8c2d
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-01 21:31:22 +0200

    feat(containers): add addchannel and init modal

--- src/containers/Sidebar/Sidebar.js
+++ src/containers/Sidebar/Sidebar.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { setCurrentChannel } from "modules/Channels";
 import { getChannels } from "modules/Channels/selectors";
+import Modal from "components/Modal";
 import Navigation from "components/Navigation";
 
 const Sidebar = () => {
   const dispatch = useDispatch();
   const channels = useSelector(getChannels);
+  const [show, setShow] = useState(false);
 
-  const handleClick = (id) => dispatch(setCurrentChannel(id));
+  const handlePlusClick = () => setShow(true);
+  const handleCloseClick = () => setShow(false);
+  const handleChannelClick = (id) => dispatch(setCurrentChannel(id));
 
-  return <Navigation items={channels} onClick={handleClick} />;
+  return (
+    <>
+      <div className="d-flex mb-2">
+        <span>Channels</span>
+        <button
+          type="button"
+          className="ml-auto p-0 btn btn-link"
+          onClick={handlePlusClick}
+        >
+          +
+        </button>
+      </div>
+      <Navigation items={channels} onClick={handleChannelClick} />
+      <Modal show={show} title="Add channel" onClose={handleCloseClick} />
+    </>
+  );
 };
 
 export default Sidebar;
