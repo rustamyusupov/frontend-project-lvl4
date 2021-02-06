8b49420c6eae1039cc053e4a33a54b7adfc0d017
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-06 14:12:14 +0200

    feat(containers): show modals by channel click for sidebar

--- src/containers/Sidebar/Sidebar.js
+++ src/containers/Sidebar/Sidebar.js
@@ -1,36 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "react-bootstrap";
 
-import { setCurrentChannel, create as createChannel } from "modules/Channels";
+import { setCurrentChannel } from "modules/Channels";
 import { getChannels } from "modules/Channels/selectors";
-import AddChannelForm from "components/AddChannelForm";
-import Form from "components/Form";
-import Modal from "components/Modal";
+import { show } from "modules/Modal";
 import Navigation from "components/Navigation";
-
-const renderModal = ({ title, show, Component, onClose, onSubmit }) => (
-  <Modal
-    title={title}
-    show={show}
-    content={
-      <Form Component={Component} onClose={onClose} onSubmit={onSubmit} />
-    }
-    onClose={onClose}
-  />
-);
 
 const Sidebar = () => {
   const dispatch = useDispatch();
   const channels = useSelector(getChannels);
-  const [show, setShow] = useState("");
 
-  const handleShow = (name) => () => setShow(name);
-  const handleClose = () => setShow("");
+  const handlePlusClick = () => dispatch(show({ type: "add" }));
   const handleChannelClick = (id) => dispatch(setCurrentChannel(id));
-  const handleAdd = (name) => dispatch(createChannel({ name }));
-  const handleRename = (name) => console.log("rename", name); // action
-  const handleRemove = (name) => console.log("remove", name); // action
+  const handleShow = (type) => (data) => dispatch(show({ type, data }));
 
   return (
     <>
@@ -39,7 +22,7 @@ const Sidebar = () => {
         <Button
           variant="link"
           className="ml-auto p-0"
-          onClick={handleShow("add")}
+          onClick={handlePlusClick}
         >
           +
         </Button>
@@ -50,28 +33,6 @@ const Sidebar = () => {
         onRename={handleShow("rename")}
         onRemove={handleShow("remove")}
       />
-      {renderModal({
-        title: "Add channel",
-        show: show === "add",
-        Component: AddChannelForm,
-        onClose: handleClose,
-        onSubmit: handleAdd,
-      })}
-      {renderModal({
-        // initialValues
-        title: "Rename channel",
-        show: show === "rename",
-        Component: AddChannelForm,
-        onClose: handleClose,
-        onSubmit: handleRename,
-      })}
-      {renderModal({
-        title: "Remove channel",
-        show: show === "remove",
-        Component: AddChannelForm,
-        onClose: handleClose,
-        onSubmit: handleRemove,
-      })}
     </>
   );
 };
