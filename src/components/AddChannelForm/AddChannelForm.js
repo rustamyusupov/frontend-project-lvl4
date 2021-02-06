b7c8a4afb083c61f975f76d336f600a11fc73468
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-06 21:34:28 +0200

    feat(components): forward ref for addchannelform

--- src/components/AddChannelForm/AddChannelForm.js
+++ src/components/AddChannelForm/AddChannelForm.js
@@ -3,27 +3,30 @@ import PropTypes from "prop-types";
 import { Form as FormikForm, Field } from "formik";
 import { Form, Button } from "react-bootstrap";
 
-const AddChannelForm = ({ errors, isSubmitting, onClose }) => (
-  <FormikForm noValidate>
-    <Form.Group className="form-group">
-      <Field
-        name="name"
-        aria-label="name"
-        className={`mb-2 form-control ${errors.name ? "is-invalid" : ""}`}
-      />
-      <Form.Control.Feedback type="invalid" className="mb-2">
-        {errors?.name}
-      </Form.Control.Feedback>
-      <div className="d-flex justify-content-end">
-        <Button className="mr-2" variant="secondary" onClick={onClose}>
-          Cancel
-        </Button>
-        <Button type="submit" disabled={isSubmitting}>
-          Submit
-        </Button>
-      </div>
-    </Form.Group>
-  </FormikForm>
+const AddChannelForm = React.forwardRef(
+  ({ errors, isSubmitting, onClose }, ref) => (
+    <FormikForm noValidate>
+      <Form.Group className="form-group">
+        <Field
+          name="name"
+          aria-label="name"
+          className={`mb-2 form-control ${errors.name ? "is-invalid" : ""}`}
+          innerRef={ref}
+        />
+        <Form.Control.Feedback type="invalid" className="mb-2">
+          {errors?.name}
+        </Form.Control.Feedback>
+        <div className="d-flex justify-content-end">
+          <Button className="mr-2" variant="secondary" onClick={onClose}>
+            Cancel
+          </Button>
+          <Button type="submit" disabled={isSubmitting}>
+            Submit
+          </Button>
+        </div>
+      </Form.Group>
+    </FormikForm>
+  )
 );
 
 AddChannelForm.propTypes = {
