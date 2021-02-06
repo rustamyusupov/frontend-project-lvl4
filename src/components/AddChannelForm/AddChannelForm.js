466e18eef428168c747ec889d25426c53618a6bd
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-06 21:47:42 +0200

    feat(components): show error for addchannelform only when touched

--- src/components/AddChannelForm/AddChannelForm.js
+++ src/components/AddChannelForm/AddChannelForm.js
@@ -4,17 +4,19 @@ import { Form as FormikForm, Field } from "formik";
 import { Form, Button } from "react-bootstrap";
 
 const AddChannelForm = React.forwardRef(
-  ({ errors, isSubmitting, onClose }, ref) => (
+  ({ errors, isSubmitting, touched, onClose }, ref) => (
     <FormikForm noValidate>
       <Form.Group className="form-group">
         <Field
           name="name"
           aria-label="name"
-          className={`mb-2 form-control ${errors.name ? "is-invalid" : ""}`}
+          className={`mb-2 form-control ${
+            touched.name && errors.name ? "is-invalid" : ""
+          }`}
           innerRef={ref}
         />
         <Form.Control.Feedback type="invalid" className="mb-2">
-          {errors?.name}
+          {touched.name && errors?.name}
         </Form.Control.Feedback>
         <div className="d-flex justify-content-end">
           <Button className="mr-2" variant="secondary" onClick={onClose}>
