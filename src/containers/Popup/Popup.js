5be2bbe4d677fb2a6e6ace3364588c4770a1cfe2
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-06 21:35:51 +0200

    feat(containers): set input focus for popup

--- src/containers/Popup/Popup.js
+++ src/containers/Popup/Popup.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
+import { Formik } from "formik";
 
+import validate from "utils/validate";
 import { create, rename, remove } from "modules/Channels";
 import { hide } from "modules/Modal";
 import { getCurrent } from "modules/Modal/selectors";
 import AddChannelForm from "components/AddChannelForm";
-import Form from "components/Form";
 import Modal from "components/Modal";
 
 const map = {
@@ -30,25 +31,35 @@ const map = {
 const Popup = ({ type }) => {
   const dispatch = useDispatch();
   const modal = useSelector(getCurrent);
+  const inputEl = useRef(null);
 
   const show = modal.show && modal.type === type;
   const { Component, title, onSubmit } = map[type];
-  const initialValues = { name: modal.data?.name ?? "" };
+
+  useEffect(() => {
+    inputEl.current?.focus();
+  }, [show]);
 
   const handleClose = () => dispatch(hide());
-  const handleSubmit = (name) => dispatch(onSubmit({ name, ...modal.data }));
+  const handleSubmit = ({ name }) => {
+    dispatch(onSubmit({ name, ...modal.data }));
+    dispatch(hide());
+  };
 
   return (
     <Modal
       show={show}
       title={title}
       content={
-        <Form
-          Component={Component}
-          initialValues={initialValues}
-          onClose={handleClose}
+        <Formik
+          initialValues={{ name: modal.data?.name ?? "" }}
+          validate={validate}
           onSubmit={handleSubmit}
-        />
+        >
+          {(props) => (
+            <Component ref={inputEl} onClose={handleClose} {...props} />
+          )}
+        </Formik>
       }
       onClose={handleClose}
     />
