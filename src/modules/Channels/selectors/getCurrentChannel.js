7ee79884fae9abc9c1de36dd6b7e3d62ac7bdd8e
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-06 13:19:53 +0200

    feat(modules): get full current channel info

--- src/modules/Channels/selectors/getCurrentChannel.js
+++ src/modules/Channels/selectors/getCurrentChannel.js
@@ -1,9 +1,10 @@
-import { get, identity } from "lodash/fp";
+import { get, find } from "lodash/fp";
 import { createSelector } from "@reduxjs/toolkit";
 
 const getCurrentChannel = createSelector(
+  get("channels.items"),
   get("channels.currentChannelId"),
-  identity
+  (items, currentId) => find({ id: currentId })(items)
 );
 
 export default getCurrentChannel;
