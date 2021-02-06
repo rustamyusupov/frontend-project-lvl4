acce4083369f57355defb223a8f537d527863b2f
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-06 14:09:05 +0200

    feat(redux): subscribe modal

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,7 +2,8 @@ import { combineReducers } from "redux";
 
 import channels from "modules/Channels";
 import messages from "modules/Messages";
+import modal from "modules/modal";
 
-const createReducer = combineReducers({ channels, messages });
+const createReducer = combineReducers({ channels, messages, modal });
 
 export default createReducer;
