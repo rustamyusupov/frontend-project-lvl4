374d1a2b1e0ce87c2cfd1ec27240d5b9731599c2
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-06 14:12:38 +0200

    feat(containers): init popup

---
+++ src/containers/Popup/Popup.js
@@ -0,0 +1,62 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+
+import { create } from "modules/Channels";
+import { hide } from "modules/Modal";
+import { getCurrent } from "modules/Modal/selectors";
+import AddChannelForm from "components/AddChannelForm";
+import Form from "components/Form";
+import Modal from "components/Modal";
+
+const map = {
+  add: {
+    Component: AddChannelForm,
+    title: "Add channel",
+    onSubmit: create,
+  },
+  rename: {
+    Component: AddChannelForm,
+    title: "Rename channel",
+    onSubmit: create,
+  },
+  remove: {
+    Component: AddChannelForm,
+    title: "Remove channel",
+    onSubmit: create,
+  },
+};
+
+const Popup = ({ type }) => {
+  const dispatch = useDispatch();
+  const modal = useSelector(getCurrent);
+
+  const show = modal.show && modal.type === type;
+  const { Component, title, onSubmit } = map[type];
+  const initialValues = { ...modal.data };
+
+  const handleClose = () => dispatch(hide());
+  const handleSubmit = (name) => dispatch(onSubmit({ name }));
+
+  return (
+    <Modal
+      show={show}
+      title={title}
+      content={
+        <Form
+          Component={Component}
+          initialValues={initialValues}
+          onClose={handleClose}
+          onSubmit={handleSubmit}
+        />
+      }
+      onClose={handleClose}
+    />
+  );
+};
+
+Popup.propTypes = {
+  type: PropTypes.oneOf(["add", "rename", "remove"]).isRequired,
+};
+
+export default Popup;
