6692dd9df2a05bd2bfb2fcc8238d4e501176010a
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-07 13:20:18 +0200

    feat(modules): set current channel when remove

--- src/modules/Channels/remove.js
+++ src/modules/Channels/remove.js
@@ -1,15 +1,23 @@
 /* eslint-disable consistent-return */
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { removeMessages } from "modules/Messages";
+import { setCurrentChannel } from "modules/Channels";
+import { getCurrentChannel } from "modules/Channels/selectors";
 
 const create = createAsyncThunk(
   "channels/remove",
-  async ({ id }, { dispatch, extra: { request } }) => {
+  async ({ id }, { dispatch, getState, extra: { request } }) => {
+    const state = getState();
+    const currentChannel = getCurrentChannel(state);
     const url = `/api/v1/channels/${id}`;
     const options = {
       method: "delete",
       params: { id },
     };
+
+    if (currentChannel.id === id) {
+      dispatch(setCurrentChannel(1));
+    }
 
     await request(url, options);
     dispatch(removeMessages(id));
