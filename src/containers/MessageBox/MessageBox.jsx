160d99675f5193ad3034a76583d6e23351be5d25
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-17 21:19:44 +0200

    feat(containers): scroll to last message after load and post

--- src/containers/MessageBox/MessageBox.jsx
+++ src/containers/MessageBox/MessageBox.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import getMessages from 'modules/Messages/selectors';
 import Chat from 'components/Chat';
 
 const MessageBox = () => {
+  const tagRef = useRef(null);
   const messages = useSelector(getMessages);
 
-  return <Chat messages={messages} />;
+  const scrollToBottom = () => tagRef.current?.scrollIntoView({ behavior: 'smooth' });
+
+  useEffect(() => {
+    const id = setTimeout(() => scrollToBottom(), 0);
+
+    return () => clearTimeout(id);
+  });
+
+  return (
+    <>
+      <Chat messages={messages} />
+      <div ref={tagRef} />
+    </>
+  );
 };
 
 export default MessageBox;
