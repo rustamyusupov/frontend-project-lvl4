c7711a414e4b894159af769d32d5310907e94c0c
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-24 21:22:42 +0200

    refactor(utils): separate getusername from context

--- src/context.jsx
+++ src/context.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import cookie from 'js-cookie';
-import faker from 'faker';
 
-const getUserName = () => {
-  const cookieName = cookie.get('name');
-
-  if (cookieName) {
-    return cookieName;
-  }
-
-  const newName = faker.internet.userName();
-
-  cookie.set('name', newName);
-
-  return newName;
-};
+import getUserName from 'utils/getUserName';
 
 const UserContext = React.createContext();
 
---
+++ src/utils/getUserName.js
@@ -0,0 +1,18 @@
+import cookie from 'js-cookie';
+import faker from 'faker';
+
+const getUserName = () => {
+  const cookieName = cookie.get('name');
+
+  if (cookieName) {
+    return cookieName;
+  }
+
+  const newName = faker.internet.userName();
+
+  cookie.set('name', newName);
+
+  return newName;
+};
+
+export default getUserName;
