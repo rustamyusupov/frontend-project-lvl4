bb3fd25e10ef17108f59fb583c01b13c6dbafe04
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-27 14:42:51 +0200

    fix(utils, en): describe required for validate

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -5,6 +5,7 @@ const maxNameLength = 20;
 
 export const channelValidationSchema = yup.object().shape({
   name: yup.string()
+    .required('required')
     .trim()
     .min(minNameLength, 'wrong length')
     .max(maxNameLength, 'wrong length'),
