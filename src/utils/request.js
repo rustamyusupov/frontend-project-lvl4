8d5297303a43cdf13f458cb6dea658d76e1e4171
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-02-27 12:46:51 +0200

    refactor(utils): optimize request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,33 +1,6 @@
 import axios from 'axios';
 import { get } from 'lodash/fp';
 
-const successfulResponses = 200;
-const noContent = 204;
-const resetContent = 205;
-const redirectionMessages = 300;
-
-const checkStatus = (response) => {
-  if (
-    response.status >= successfulResponses
-    && response.status < redirectionMessages
-  ) {
-    return response;
-  }
-
-  const error = new Error(response.statusText);
-
-  error.response = response;
-  throw error;
-};
-
-const parse = (response) => {
-  if (response.status === noContent || response.status === resetContent) {
-    return null;
-  }
-
-  return get('data')(response);
-};
-
-const request = (url, options) => axios(url, options).then(checkStatus).then(parse);
+const request = (url, options) => axios(url, options).then(get('data'));
 
 export default request;
