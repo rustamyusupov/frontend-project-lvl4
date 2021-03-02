bc576c1d1f1985c8c56d7b6d5139c1c9c9081527
Rustam Yusupov <rustam.yusupov@gmail.com>
2021-03-02 20:42:14 +0200

    refactor(slices): rename create message action

--- src/slices/messages/slice.js
+++ src/slices/messages/slice.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 
-import create from './thunk';
+import createMessage from './thunk';
 
 const messages = createSlice({
   name: 'messages',
@@ -20,7 +20,7 @@ const messages = createSlice({
     },
   },
   extraReducers: {
-    [create.rejected]: (state, action) => {
+    [createMessage.rejected]: (state, action) => {
       state.error = action.error.message;
     },
   },
